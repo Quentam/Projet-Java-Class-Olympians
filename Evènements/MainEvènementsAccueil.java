f402292ebf605d924acfedd3c3f5ed4a68e0d6ca
Adrithub <151947603+Adrithub@users.noreply.github.com>
2024-05-29 22:52:38 +0200

    Create MainEvènementsAccueil.java

---
+++ Evènements/MainEvènementsAccueil.java
@@ -0,0 +1,29 @@
+package fr.isep.algo.classolympians;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class MainEvènementsAccueil extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Charger le fichier FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fr/isep/algo/classolympians/EvènementsAccueil.fxml"));
+        Parent root = loader.load();
+
+        // Créer et afficher la scène
+        primaryStage.setTitle("Evènements Accueil");
+        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
