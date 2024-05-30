4c8b82d337345f9bc09f1867bca7b40ca594cec4
Quentam <154077469+Quentam@users.noreply.github.com>
2024-05-30 14:46:28 +0200

    Update ShareDataAthlètes.java

--- Athlètes/ShareDataAthlètes.java
+++ Athlètes/ShareDataAthlètes.java
@@ -7,11 +7,8 @@ public class ShareDataAthlètes {
     private static List<Athlète> athlètes = new ArrayList<>();
 
     static {
-        athlètes.add(new Athlète("Riner", "Teddy", "France", "38", "Judo"));
-        athlètes.add(new Athlète("Mayer", "Kevin", "France", "30", "Décathlon"));
-        athlètes.add(new Athlète("Lavillenie", "Renaud", "France", "34", "Saut à la perche"));
-        athlètes.add(new Athlète("Mbappé", "Kylian", "France", "23", "Football"));
-        athlètes.add(new Athlète("Marchand", "Léon", "France", "20", "Natation"));
+        athlètes.add(new Athlète("Mbappé", "Kylian", "France", 23, 'M',"Football"));
+        athlètes.add(new Athlète("Marchand", "Léon", "France", 20,'M', "Natation"));
     }
 
     public static List<Athlète> getAthlètes() {
