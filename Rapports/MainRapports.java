b518a920e32470ffc94626c910f1c113e9dc0b9c
AntoineBillot <156093129+AntoineBillot@users.noreply.github.com>
2024-05-30 18:10:25 +0200

    Create MainRapports.java

---
+++ Rapports/MainRapports.java
@@ -0,0 +1,27 @@
+package fr.isep.algo.accueilprojet;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import java.awt.Toolkit;
+
+public class MainRapports extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fr/isep/algo/classolympians/RapportsAccueil.fxml"));
+        Scene scene = new Scene(root, Toolkit.getDefaultToolkit().getScreenSize().getWidth(), Toolkit.getDefaultToolkit().getScreenSize().getHeight());
+        primaryStage.setTitle("Page Rapports");
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
